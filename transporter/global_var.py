77aff710481affae0c80bc0c05ed3b9967ab6709
Lee-DaeSeong <87695174+Lee-DaeSeong@users.noreply.github.com>
2022-04-13 17:07:28 +0900

    Update global_var.py

--- transporter/global_var.py
+++ transporter/global_var.py
@@ -3,6 +3,7 @@ work_stime = 8*60*60    # 작업 시작 시각
 work_etime = 23*60*60   # 작업 마감 시각
 load_time = 15*60       # 로드하는데 걸리는 시간
 unload_time = 15*60     # 언로드하는데 걸리는 시간
+# test
 
 # 염색체 관련 변수
 populationSize = 10     # 한 세대당 염색체 수
